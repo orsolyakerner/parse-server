cfd37535c2b0fac47efe065074312660d07178dd
Kerner Orsolya <orsolya.kerner@blackswan.com>
2016-12-14 16:12:07 +0100

    Ignore to send verification email if the user has been registered/logged in via Facebook

--- src/Controllers/UserController.js
+++ src/Controllers/UserController.js
@@ -112,7 +112,7 @@ export class UserController extends AdaptableController {
   }
 
   sendVerificationEmail(user) {
-    if (!this.shouldVerifyEmails) {
+    if (!this.shouldVerifyEmails || typeof user.authData !== 'undefined') {
       return;
     }
     const token = encodeURIComponent(user._email_verify_token);
